7ccf774ed273fd70390cfa05f5f89ce23155d30d
SharpIce <VupRbl@gmail.com>
2022-07-07 22:57:40 +0800

    添加了三把武器

--- src/main/java/com/sharpice/sow/items/Items.java
+++ src/main/java/com/sharpice/sow/items/Items.java
@@ -5,10 +5,15 @@ import net.minecraft.util.registry.Registry;
 
 import static com.sharpice.sow.Util.MODID;
 import static com.sharpice.sow.items.Axe_Stubby.AXE_STUBBY;
+import static com.sharpice.sow.items.Dagger_Stone.DOGGER_STONE;
+import static com.sharpice.sow.items.dagger_diamond.DAGGER_DIAMOND;
+import static com.sharpice.sow.items.sword1_iron.SWORD1_IRON;
 
 public class Items {
-    public static void register()
-    {
-        Registry.register(Registry.ITEM, new Identifier(MODID, "axe_stubby"), AXE_STUBBY); //注册Axe_Stubby
+    public static void register() {
+        Registry.register(Registry.ITEM, new Identifier(MODID, "axe_stubby"), AXE_STUBBY);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "dagger_stone"), DOGGER_STONE);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "dagger_diamond"), DAGGER_DIAMOND);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "sword1_iron"), SWORD1_IRON);
     }
 }
---
+++ src/main/java/com/sharpice/sow/items/sword1_iron.java
@@ -0,0 +1,38 @@
+package com.sharpice.sow.items;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolItem;
+import net.minecraft.recipe.Ingredient;
+
+import static com.sharpice.sow.Group.SOW_GROUP;
+
+public class sword1_iron {
+    public static final Axe_Stubby INSTANCE = new Axe_Stubby();
+    public static final ToolItem SWORD1_IRON = new SwordItem(INSTANCE, 1, 1.8F, new Item.Settings().group(SOW_GROUP)); // 定义一个新的斧
+
+    public int getDurability() { //耐久
+        return 1000;
+    }
+
+    public float getMiningSpeedMultiplier() { // 挖掘速度
+        return 1.5f;
+    }
+
+    public float getAttackDamage() { // 攻击伤害
+        return 1.0F;
+    }
+
+    public int getMiningLevel() { // 挖掘等级
+        return 1;
+    }
+
+    public int getEnchantability() { // 附魔等级
+        return 10;
+    }
+
+    public Ingredient getRepairIngredient() { // 修复材料
+        return Ingredient.ofItems(Blocks.COBBLESTONE);
+    }
+}
